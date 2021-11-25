f49774aabe52fee78479934ef2f87fbea1c37f7e
Johannes Schütz <suetzjoh@hu-berlin.de>
2021-11-25 11:02:46 +0100

    leerzeichen

--- perfekt_reverse.py
+++ perfekt_reverse.py
@@ -3,6 +3,7 @@ import re, regex, codecs
 import csv
 
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 git_dir_korr = "D:\git\korrektur"
@@ -24,39 +25,107 @@ for f in files:
 with open(perfekt_file, 'r', encoding="UTF-8-SIG") as csvfile:
 	print("loading database")
 	reader = csv.reader(csvfile, dialect='excel', delimiter=';')
-	db = [row for row in reader]
-	db.sort(key=lambda x: (int(x[0]), int(x[1]), int(x[2]))) # part, page, line
+	perfekta = [row for row in reader if len(row) > 1]
+	perfekta.sort(key=lambda x: (int(x[0]), int("".join([ch for ch in x[1] if ch.isdigit()])), int(x[2]))) # part, page, line
 
 log = ""
 def writelog():
 	with open("log.txt", "w", encoding="UTF-8") as file:
 		file.write(log)
 
+cite_stat = 0
+i = 0
+
+log = ""
+
 pattern = "(?|^()({match})([ {{}}])|([ {{}}])({match})([ {{}}])|([ {{}}])({match})()$)"	
 old_part = ""
 # = [["part", "page", "line", "subpart", "match", "safeness", "Case", "Kommentar"]]
-for i in range(len(db)):
-	perfekt = db[i]
-	if i % 200 == 0:
-		print(perfekt[:3], "   ", end="\r")
-	
-	part = library.library.find("document", attrs={"nr" : re.compile("^{}({{\d+}})?".format(perfekt[0]))})
-	page = part.find("lpp", attrs={"nr" : re.compile("^{}({{\d+}})?".format(perfekt[1]))})
-	line = page.find("z", attrs={"nr" : re.compile("^{}({{\d+}})?".format(perfekt[2]))})
-	
-	if not perfekt[0] == old_part:
-		print("annotating part", perfekt[0], " ")
-		old_part = perfekt[0]
-	
-	text = line.text
-	quant = len(regex.findall(pattern.format(match=perfekt[4]), text))
-	if not quant == 1:
-		log += "emergency: " + str(quant) + str(perfekt) + "\n" + text + "\n\n"
-		writelog()
+for part in library.library.find_all("document", attrs={"nr" : re.compile("^\d+?({\d+})?")}):
+	part_nr = part["nr"]
+	for page in part.find_all("lpp", attrs={"nr" : re.compile("^\d+?({{\d+}}|a|b)?")}):
+		page_nr = re.search("\d+", page["nr"]).group(0)
 		
-	text = regex.sub(pattern.format(match=perfekt[4]), "\g<1> <match ann=\"{ann}\">\g<2></match> \g<3>".format(ann=perfekt[7]), text, re.IGNORECASE)
-	line.string.replace_with(text)
-	
+		log = re.sub("^[\s\S]+(page {})".format(str(int(page_nr)-1)), "", log)
+		log += "\n" + "page " + page_nr + "\n"
+		
+		for line in page.find_all("z", attrs={"nr" : re.compile("^\d+?({{\d+}})?")}):
+			line_nr = line["nr"]
+			
+			if len(perfekta) == 0:
+				break
+			
+			log += "\n" + part_nr + "," + page_nr + "," + line_nr + " " + line.text + "\n"
+			text = line.text
+			text = re.sub("[{}]", " \g<0> ", text)
+			text = text.split()
+			
+			if line.text == "":
+				continue
+				
+			matches = []
+			for row in perfekta:
+				if row[:2] == perfekta[0][:2]:
+					row[4] = re.sub(".+?-", "", row[4])
+					matches.append(row[:5])
+					#matches.append(re.sub(".+?-", "", row).casefold())
+				else:
+					break
+								
+			
+			log += str(matches) + "\n"
+			matches = [row[4] for row in matches]
+			
+			newstring = ""
+			
+			int_page_nr = "".join([ch for ch in page_nr if ch.isdigit()])
+			if int(part_nr) > int(perfekta[0][0]) or int(part_nr) == int(perfekta[0][0]) and int(int_page_nr) > int(perfekta[0][1]): #or int(part_nr) == int(perfekta[0][0]) and int(int_page_nr) == int(perfekta[0][1]) and int(line_nr) > int(perfekta[0][2]):
+				print(log)
+				input("emergency")
+			
+			for word in text:
+				if len(perfekta) == 0:
+					break
+				
+				log += word + " " + str(word in matches) + "\n"
+				match = re.sub(".+?-", "", perfekta[0][4])
+				
+				if word in matches:
+					ii = matches.index(word)
+					
+					#log += str(ii) + "\n"
+					
+					if perfekta[ii][:3] != [part_nr, page_nr, line_nr]:
+						continue
+					
+					newstring += "<match ann=\"{ann}\">".format(ann=perfekta[ii][7], cit=cite_stat) + word + "</match> "
+					#newstring += "<match ann=\"{ann}\" zit=\"{cit}\">".format(ann=perfekta[ii][7], cit=cite_stat) + match + "</match> "
+					
+					if not perfekta[0][0] == old_part:
+						print("annotating part", perfekta[0][0], " ")
+						old_part = perfekta[0][0]
+					
+					i += 1
+					if i % 200 == 0:
+						print(perfekta[0][:3], "   ", end="\r")
+					
+					#log += perfekta[ii][4] 
+					
+					perfekta.pop(ii)
+					matches.pop(ii)
+					
+					#log += "popped " + str(matches) + "\n"
+					
+				else:
+					newstring += word + " "
+					
+					if word in ["„", "‚"]:
+						cite_stat += 1
+					elif word in ["“", "‘"]:
+						cite_stat -= 1
+			
+			log += newstring + "\n"
+			line.string.replace_with(newstring)
 
 with open("perfekt.xml", "w", encoding="utf-8-sig") as file:
 	string = str(library)
